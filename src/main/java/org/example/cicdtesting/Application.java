233061e619446e857834d883f10e48f15e198f03
Johannes <98045373+Luxcorel@users.noreply.github.com>
2024-01-19 19:48:33 +0100

    This should break build

--- src/main/java/org/example/cicdtesting/Application.java
+++ src/main/java/org/example/cicdtesting/Application.java
@@ -3,11 +3,4 @@ package org.example.cicdtesting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-}
